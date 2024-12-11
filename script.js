c1bd3760d98e9ac3d2d4a66053cfb11be2380f17
mart5-jpg <martinsupenieks9@gmail.com>
2024-12-11 10:25:24 +0200

    Pievienoju failus

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button')
+
+const buttonHeight = 100;
+const buttonWidth = 250;
+
+button.addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=2qgxAHW1w78", "_blank");
+})
